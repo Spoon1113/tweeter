1da241775d43fb9617a2f86ec18b39948e7240b9
Samson Poon <samsonkfpoon@gmail.com>
2023-02-01 02:00:22 +0000

    added char counter, turns red if it exceeds character limit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $("#tweet-text").on("input", function () {
+    let input = $(this).val().length;
+    let charCount = 140 - input;
+    console.log("text length" , charCount);
+    let counter = $(this).parent().find(".counter");
+    counter.html(charCount);
+
+    if (charCount < 0) {
+      counter.addClass("redChar")
+    } else {
+      counter.removeClass("redChar")
+    }
+  });
+});
